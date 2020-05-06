044d94de9108a17350daea574df1579e4e3082b6
Leonardo Briones <leobrionesherrera@hotmail.com>
2020-05-06 18:19:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   arr = string.split
   string2 = String.new
   arr.each do |word|
-    string2 << word.reverse
+    string2 << "#{word.reverse} "
   end
   string2
 end
